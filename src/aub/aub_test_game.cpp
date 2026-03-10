365a71109e09a7002d0efc98768e68d1b1c10d0d
JHud <219741840+JamHudson@users.noreply.github.com>
2026-03-10 17:19:38 +0000

    Reverted change to aub_test_game

--- src/aub/aub_test_game.cpp
+++ src/aub/aub_test_game.cpp
@@ -7,18 +7,17 @@
 
 #include "mj/mj_game_list.h"
 
-
 // String arrays for the credits can go in an anonymous namespace
 namespace
 {
-    constexpr bn::string_view code_credits[] = { "Auberon Lopez" };
-    constexpr bn::string_view graphics_credits[] = { "Kenney Game Assets" };
+    constexpr bn::string_view code_credits[] = {"Auberon Lopez"};
+    constexpr bn::string_view graphics_credits[] = {"Kenney Game Assets"};
     constexpr bn::string_view sfx_credits[] = {""};
     constexpr bn::string_view music_credits[] = {""};
 }
 
 // Macros used to add game to game list
-/*MJ_GAME_LIST_ADD(aub::aub_test_game)*/ // The class of the game is used here
+MJ_GAME_LIST_ADD(aub::aub_test_game) // The class of the game is used here
 MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
 MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
 MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
@@ -28,84 +27,87 @@ MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
 namespace aub
 {
 
-/**
- * Constructor for an instance of an aub_test_game
- * 
- * First item in the initializer list MUST be a call to the superclass, mj::game with the identifier for the microgame.
- * 
- * @param completed_games how many microgames the player has completed so far
- * @param data shared information, such as a rng and number of frames left in the microgame
- */
-aub_test_game::aub_test_game([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
-    mj::game("aub"),
-    _player(player({20, 0}, 2))
-    {}
+    /**
+     * Constructor for an instance of an aub_test_game
+     *
+     * First item in the initializer list MUST be a call to the superclass, mj::game with the identifier for the microgame.
+     *
+     * @param completed_games how many microgames the player has completed so far
+     * @param data shared information, such as a rng and number of frames left in the microgame
+     */
+    aub_test_game::aub_test_game([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data &data) : mj::game("aub"),
+                                                                                                                     _player(player({20, 0}, 2))
+    {
+    }
 
-/**
- * The instructions given to the player at the beginning of the microgame.
- * 
- * Must be <= 16 characters long
- */
-bn::string<16> aub_test_game::title() const {
-    return "Leave the screen";
-}
+    /**
+     * The instructions given to the player at the beginning of the microgame.
+     *
+     * Must be <= 16 characters long
+     */
+    bn::string<16> aub_test_game::title() const
+    {
+        return "Leave the screen";
+    }
 
-/**
- * How long the timer for the game should be set to in frames.
- * 
- * GBA runs at approx 60 frames per second.
- */
-int aub_test_game::total_frames() const {
-    return 300; // 300 frames at 60fps = 5 seconds
-}
+    /**
+     * How long the timer for the game should be set to in frames.
+     *
+     * GBA runs at approx 60 frames per second.
+     */
+    int aub_test_game::total_frames() const
+    {
+        return 300; // 300 frames at 60fps = 5 seconds
+    }
 
-/**
- * play is repeatedly called while the microgame is playing.
- * 
- * You can think of it as what previously was in the while event loop.
- * You do NOT need to write your own while event loop
- * You do NOT need to call bn::core::update()
- * 
- * @param data shared information, such as a rng and number of frames left in the microgame
- * @return the mj::game result indicating whether the game has finished and whether the title should be hidden
- */
-mj::game_result aub_test_game::play([[maybe_unused]] const mj::game_data& data)
-{
-    // update the player position
-    _player.update();
+    /**
+     * play is repeatedly called while the microgame is playing.
+     *
+     * You can think of it as what previously was in the while event loop.
+     * You do NOT need to write your own while event loop
+     * You do NOT need to call bn::core::update()
+     *
+     * @param data shared information, such as a rng and number of frames left in the microgame
+     * @return the mj::game result indicating whether the game has finished and whether the title should be hidden
+     */
+    mj::game_result aub_test_game::play([[maybe_unused]] const mj::game_data &data)
+    {
+        // update the player position
+        _player.update();
 
-    // Creates a game result indicating whether the game is finished and whether the title should be hidden early
-    // For this game the game should end early if the player has won (if victory returns true)
-    // The title is not hidden early (false is passed), so the title disappears at the default time
-    mj::game_result result(victory(), false);
-    return result;
-}
+        // Creates a game result indicating whether the game is finished and whether the title should be hidden early
+        // For this game the game should end early if the player has won (if victory returns true)
+        // The title is not hidden early (false is passed), so the title disappears at the default time
+        mj::game_result result(victory(), false);
+        return result;
+    }
 
-/**
- * Returns whether the player has won the microgame.
- * 
- * In this particular microgame the player wins if they make the ball leave the screen.
- */
-bool aub_test_game::victory() const {
-    return _player.out_of_bounds();
-}
+    /**
+     * Returns whether the player has won the microgame.
+     *
+     * In this particular microgame the player wins if they make the ball leave the screen.
+     */
+    bool aub_test_game::victory() const
+    {
+        return _player.out_of_bounds();
+    }
 
-/**
- * Called repeatedly as the game fades into view. Unused for this particular microgame.
- * 
- * @param data shared information, such as a rng and number of frames left in the microgame
- */
-void aub_test_game::fade_in([[maybe_unused]] const mj::game_data& data)
-{
-}
+    /**
+     * Called repeatedly as the game fades into view. Unused for this particular microgame.
+     *
+     * @param data shared information, such as a rng and number of frames left in the microgame
+     */
+    void aub_test_game::fade_in([[maybe_unused]] const mj::game_data &data)
+    {
+    }
 
-/**
- * Called repeatedly as the game fades into view. Unused for this particular microgame.
- * 
- * @param data shared information, such as a rng and number of frames left in the microgame
- */
-void aub_test_game::fade_out([[maybe_unused]] const mj::game_data& data)
-{
-}
+    /**
+     * Called repeatedly as the game fades into view. Unused for this particular microgame.
+     *
+     * @param data shared information, such as a rng and number of frames left in the microgame
+     */
+    void aub_test_game::fade_out([[maybe_unused]] const mj::game_data &data)
+    {
+    }
 
 }
