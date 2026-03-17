daf7ccb1efab9aaec5adba73b486dabd265711cd
BushyMustache <205280148+BushyMustache@users.noreply.github.com>
2026-03-17 18:51:06 +0000

    Resolved rest of merge conflicts

--- include/aub/aub_test_game.h
+++ include/aub/aub_test_game.h
@@ -86,7 +86,6 @@ class aub_test_game : public mj::game
         // Returns progressively slower player speeds the harder the difficulty
         // The slower the player moves, the harder it is to leave the screen before the timer ends
         bn::fixed _recommended_player_speed(mj::difficulty_level difficulty);
-        bn::regular_bg_ptr _background;
 };
 
 }
